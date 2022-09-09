7f043aa24e465a711571b201628104164cbaf13c
luamfb <luamfb@riseup.net>
2022-09-09 20:17:16 -0300

    Implement proper expressions

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,62 @@
+#[derive(Debug, PartialEq, Clone)]
 pub enum Expr<'a> {
     Id(&'a str),
     Lit(Literal<'a>),
+    UnOp(Box<Expr<'a>>, UnaryOper),
+    BinOp(Box<Expr<'a>>, BinaryOper, Box<Expr<'a>>),
+    Ternary {
+        cond: Box<Expr<'a>>,
+        if_val: Box<Expr<'a>>,
+        else_val: Box<Expr<'a>>
+    },
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum UnaryOper {
+    Plus,
+    Minus,
+    Not,
+    BitNot,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum BinaryOper {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Rem,
+    Equals,
+    NotEquals,
+    Less,
+    LessEq,
+    Greater,
+    GreaterEq,
+    And,
+    Or,
+    BitAnd,
+    BitOr,
+    BitXor,
+    ShiftLeft,
+    ShiftRight,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum AssignOper {
+    Assign,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    RemAssign,
+    BitAndAssign,
+    BitOrAssign,
+    BitXorAssign,
+    ShiftLeftAssign,
+    ShiftRightAssign,
+}
+
+#[derive(Debug, PartialEq, Clone)]
 pub enum Literal<'a> {
     Str(&'a str),
     Dec(&'a str),
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,94 @@
-use lalrpop_util::lalrpop_mod;
+use lalrpop_util::{lalrpop_mod, ParseError};
+use crate::{
+    ast::Expr,
+    lexer::{Lexer, Token},
+};
 
-lalrpop_mod!(pub grammar);
+lalrpop_mod!(grammar);
+
+type Error<'a> = ParseError<usize, Token<'a>, ()>;
+
+pub fn parse_expr<'a>(src: &'a str) -> Result<Expr<'a>, Error<'a>> {
+    let lexer = Lexer::new(src);
+    let expr = grammar::ExprParser::new()
+        .parse(src, lexer);
+    expr
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        ast::*,
+    };
+
+    #[test]
+    fn expr_id() {
+        let src = "x";
+        let expected = Expr::Id("x");
+        assert_eq!(expected, parse_expr(src).unwrap());
+    }
+
+    #[test]
+    fn expr_num() {
+        let src = "0";
+        let expected = Expr::Lit(Literal::Dec("0"));
+        assert_eq!(expected, parse_expr(src).unwrap());
+    }
+
+    #[test]
+    fn expr_strlit() {
+        let src = "'foobar'";
+        let expected = Expr::Lit(Literal::Str("'foobar'"));
+        assert_eq!(expected, parse_expr(src).unwrap());
+    }
+
+    #[test]
+    fn single_bin_expr() {
+        let src = "x + 2";
+        let expected = Expr::BinOp(
+            Box::new(Expr::Id("x")),
+            BinaryOper::Add,
+            Box::new(Expr::Lit(Literal::Dec("2"))),
+        );
+        assert_eq!(expected, parse_expr(src).unwrap());
+    }
+
+    #[test]
+    fn single_unary_expr() {
+        let src = "+a";
+        let expected = Expr::UnOp(Box::new(Expr::Id("a")), UnaryOper::Plus);
+        assert_eq!(expected, parse_expr(src).unwrap());
+    }
+
+    #[test]
+    fn left_associative() {
+        let src = "a + b + c + 1";
+        let expected = Expr::BinOp(
+            Box::new(Expr::BinOp(
+                    Box::new(Expr::BinOp(
+                            Box::new(Expr::Id("a")),
+                            BinaryOper::Add,
+                            Box::new(Expr::Id("b")))),
+                    BinaryOper::Add,
+                    Box::new(Expr::Id("c")))),
+            BinaryOper::Add,
+            Box::new(Expr::Lit(Literal::Dec("1"))));
+        assert_eq!(expected, parse_expr(src).unwrap());
+    }
+
+    #[test]
+    fn mult_add_precedence() {
+        let src = "a + b * c";
+        let expected = Expr::BinOp(
+            Box::new(Expr::Id("a")),
+            BinaryOper::Add,
+            Box::new(Expr::BinOp(
+                    Box::new(Expr::Id("b")),
+                    BinaryOper::Mul,
+                    Box::new(Expr::Id("c")))));
+        assert_eq!(expected, parse_expr(src).unwrap());
+    }
+
+    // TODO: more expression tests
+}
